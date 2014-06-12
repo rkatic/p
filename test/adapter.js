ffccbf2d0c451d0bede45151d1633e997c21d9ff
Robert Katic <robert.katic@gmail.com>
2014-06-12 01:10:27 -0400

    Update to promise-aplus-tests ~2

--- test/adapter.js
+++ test/adapter.js
@@ -1,12 +1,6 @@
 
 var P = require('../p');
 
-exports.pending = function() {
-	var d = P.defer();
-
-	return {
-		promise: d.promise,
-		fulfill: d.resolve,
-		reject: d.reject
-	};
-};
+exports.resolved = P;
+exports.rejected = P.reject;
+exports.deferred = P.defer;
